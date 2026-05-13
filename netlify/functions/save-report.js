a45e20a7f7744ad09e9d45963abb2fb68f1366b1
zakces25-stack <zakces25@gmail.com>
2026-05-13 16:29:07 +0100

    Update token environment variable to NETLIFY_API_TOKEN

--- netlify/functions/save-report.js
+++ netlify/functions/save-report.js
@@ -46,7 +46,7 @@ exports.handler = async (event) => {
     try {
       const store = getStore('reports', {
         siteID: process.env.NETLIFY_SITE_ID,
-        token:  process.env.NETLIFY_TOKEN,
+        token:  process.env.NETLIFY_API_TOKEN,
       });
       const htmlContent = Buffer.from(fileContent, 'base64').toString('utf8');
       await store.set(reportId, htmlContent);
